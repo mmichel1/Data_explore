8e29e40b1740ab08335267e5492c1eaef58b1ad4
Hans Karlshoefer <hans.karlshoefer@atos.net>
2018-05-10 08:18:56 +0200

    Update of numerical plots with ggplot2

--- Functions_data_exploration.R
+++ Functions_data_exploration.R
@@ -4,13 +4,62 @@
 # Exploration: general functions
 #----------------------------------------------
 
+#library(tidyverse)
+#library(hexbin)
+
+# ====================== Function ================
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
 
 # ====================== Function ================
 # Print of boxplots, histograms and summaries for numerical variables
 plot_num <- function(df,desc) {
-  par(mfrow=c(1,2), mai=c(0.5,0.5,0.5,0.5), pty="m", cex=0.8)
+  #par(mfrow=c(1,2), mai=c(0.5,0.5,0.5,0.5), pty="m", cex=0.8)
   #'mai' gives the number of lines of margin
-  # to be specified on the four sides of the plot. 
+  # to be specified on the four sides of the plot.
   for(i in 1:ncol(df)) {
     if (all (is.na(df_num[,i])) == TRUE) {
       print("--------------------------------------------------------------------------------------------")
@@ -18,17 +67,38 @@ plot_num <- function(df,desc) {
       print(paste("Comment   : all values na ",var_desc$Comment.Data.Expl))
     }    
     else {
-      # Boxplots
-      boxplot(df[,i], main=names(df[i]), type="l")
-      text(y=fivenum(df[,i]), labels =fivenum(round(df[,i], 3)), x=1.25)
-      # Histograms
-      hist(df[,i], main=names(df[i]), xlab=NULL)
+      # --- Boxplots ---
+      boxp_5_num <- fivenum(df[,i]) # gives the 5 numbers
+      p1 <- ggplot(data = df) +
+        geom_boxplot(mapping = aes(x = "", y = df[,i]), na.rm = TRUE) +
+        labs(x = names(df[i]), y = "") +
+        geom_text(data = as.data.frame(boxp_5_num), aes(x = 1.25, boxp_5_num, label = boxp_5_num))
+      #boxplot(df[,i], main=names(df[i]), type="l")
+      #text(y=fivenum(df[,i]), labels =fivenum(round(df[,i], 3)), x=1.25)
+      
+      # --- Histograms ---
+      #hist(df[,i], main=names(df[i]), xlab=NULL)
+      if (max(na.omit(df_num[i])) - min(na.omit(df_num[i])) == 0) {
+        bw <- 0.1
+      }
+      else {
+        bw <- (max(na.omit(df_num[i])) - min(na.omit(df_num[i])))/30
+      }
+      p2 <- ggplot(data = df) +
+        geom_histogram(mapping = aes(x = df[,i]), binwidth = bw, na.rm = TRUE) +
+        labs(x = names(df[i]))
+      
+      # --- plot ---
+      multiplot(p1, p2, cols=2)
+      #gridextra::gridarrange (p1, p2, cols=2)
       # Summaries
       print(summary(df[i]))
-      print(paste("# of na    :", colSums(is.na(df[i])), "  in %", colSums(is.na(df[i]))/nrow(df[i])*100))
+      print(paste("# of na in %:", round(colSums(is.na(df[i]))/nrow(df[i])*100)), 1)
+      print(paste("Variance    :", var(na.omit(df_num[,i]))))     
+      print(paste("Standard dev:", sqrt(var(na.omit(df_num[,i])))))    
       var_desc <-(desc[names(df[i])==desc[,1],]) # get descriptions of the field
-      print(paste("Description:", var_desc[2]))  # field description
-      print(paste("Comment    :", var_desc$Comment.Data.Expl)) # field comment
+      print(paste("Description :", var_desc[2]))  # field description
+      print(paste("Comment     :", var_desc$Comment.Data.Expl)) # field comment
     }
   }
 }
@@ -101,3 +171,4 @@ check_collinearity <- function(df) {
     }
   }
 }
+
